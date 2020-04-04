7b8d188020ed13aa8dbf25c5b9b6cd016a581f71
yehsangoh <jaws0910@gmail.com>
2020-04-04 13:41:11 +0900

    HelloContrroller에 HelloResponseDto 적용 및 Test 코드 작성

--- src/main/java/com/sangoh/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/sangoh/book/springboot/web/dto/HelloResponseDto.java
@@ -3,6 +3,7 @@ package com.sangoh.book.springboot.web.dto;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @RequiredArgsConstructor
--- src/test/java/com/sangoh/book/springboot/web/HelloContrrollerTest.java
+++ src/test/java/com/sangoh/book/springboot/web/HelloContrrollerTest.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloContrroller.class)
@@ -25,7 +26,19 @@ public class HelloContrrollerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "dobby";
+        int amount = 10;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.amount").value(amount));
     }
 
 }
