24d1b7b7616e2c21b0ba84b85abe6468e933613c
yehsangoh <jaws0910@gmail.com>
2020-04-10 12:54:55 +0900

    Posts 조회, 삭제 수정 추가

--- src/main/java/com/sangoh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sangoh/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.sangoh.book.springboot.service.posts;
 
 import com.sangoh.book.springboot.domain.posts.Posts;
 import com.sangoh.book.springboot.domain.posts.PostsRepository;
+import com.sangoh.book.springboot.web.dto.PostsListResponseDto;
 import com.sangoh.book.springboot.web.dto.PostsResponseDto;
 import com.sangoh.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sangoh.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +35,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(entity);
     }
 }
--- src/main/java/com/sangoh/book/springboot/web/IndexController.java
+++ src/main/java/com/sangoh/book/springboot/web/IndexController.java
@@ -1,17 +1,35 @@
 package com.sangoh.book.springboot.web;
 
+import com.sangoh.book.springboot.service.posts.PostsService;
+import com.sangoh.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
         // "Index"만 리턴해도 머스터치 스타터 덕분에 앞뒤로 경로가 붙는다.
         // 그리고 그렇게 전달된 mustache 파일은 view resolver가 처리해준다.
         // result: /src/main/resources/templates/index.mustache
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 
     @GetMapping("/posts/save")
