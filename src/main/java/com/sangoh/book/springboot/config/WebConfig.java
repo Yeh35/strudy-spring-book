1a10304b8cc6192c2eaab4e493ca0782ab607554
yehsangoh <jaws0910@gmail.com>
2020-04-11 11:11:29 +0900

    구글 로그인 및 로그인 세션 이노베이션 추가

---
+++ src/main/java/com/sangoh/book/springboot/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sangoh.book.springboot.config;
+
+import com.sangoh.book.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configurable
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+
+}
--- src/main/java/com/sangoh/book/springboot/web/IndexController.java
+++ src/main/java/com/sangoh/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sangoh.book.springboot.web;
 
+import com.sangoh.book.springboot.config.auth.LoginUser;
+import com.sangoh.book.springboot.config.auth.dto.SessionUser;
 import com.sangoh.book.springboot.service.posts.PostsService;
 import com.sangoh.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         // "Index"만 리턴해도 머스터치 스타터 덕분에 앞뒤로 경로가 붙는다.
         // 그리고 그렇게 전달된 mustache 파일은 view resolver가 처리해준다.
